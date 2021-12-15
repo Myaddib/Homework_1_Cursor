96faa41f27d44fadb7405864ee6d34495d623d47
Myaddib <Hotabich80@gmail.com>
2021-12-15 10:45:58 +0200

    Initial commit

---
+++ src/basic/java_cursor_education/Main.java
@@ -0,0 +1,24 @@
+package basic.java_cursor_education;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int x,y,a,w,b,e;
+        x = 3;
+        y = 6;
+        a = 7;
+        w = 8;
+        b = 10;
+        e = 5;
+
+        //    x+y-a/2*(x+w)
+        System.out.println(x+y-a/2*(x+w));
+        //    a*(x+x)/y-a*3
+        System.out.println( a*(x+x)/y-a*3);
+        //    a+b/x-y
+        System.out.println(a+b/x-y);
+        //    X-e/2%e+1
+        System.out.println(x-e/2%e+1);
+    }
+}
